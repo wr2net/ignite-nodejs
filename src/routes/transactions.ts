2b75373110292e5f316b53200c0c6111b16dd60a
wr2net <wagnerr2@gmail.com>
2023-11-10 07:52:26 -0300

    feat: ajustando rota list e criando rota show transaction.

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,46 @@ import {FastifyInstance} from "fastify";
 import {z} from "zod";
 
 export async function transactionsRoutes(app: FastifyInstance) {
+
+	/*
+	Method: GET
+	Index
+	List Transactions
+	 */
+	app.get('/', async () => {
+		const transactions = await knex('transactions').select();
+		return {
+			transactions,
+		}
+	});
+
+	/*
+	Method: GET
+	Show
+	Show a Transactions
+	 */
+	app.get('/:id', async (request, response) => {
+		const getTransactionParamsSchema = z.object({
+			id: z.string().uuid(),
+		})
+		const { id } = getTransactionParamsSchema.parse(request.params)
+		const transaction = await knex('transactions').where('id', id).first()
+
+		if (transaction === undefined) {
+			return response.status(404).send({
+				'status': 404,
+				'message': 'Transaction not found!'
+			})
+		}
+
+		return response.status(200).send({transaction})
+	})
+
+	/*
+	Method: GET
+	Store
+	Persist a Transactions
+	 */
 	app.post('/', async (request, response) => {
 
 		const createTransactionBodySchema = z.object({
@@ -23,7 +63,5 @@ export async function transactionsRoutes(app: FastifyInstance) {
 		return response.status(201).send()
 	});
 
-	app.get('/', async () => {
-		return knex('transactions').select('*');
-	});
+
 }
