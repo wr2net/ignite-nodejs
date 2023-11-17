e6c7387a6a6f9163ab350f842a944f2bb8bdcb66
wr2net <wagnerr2@gmail.com>
2023-11-17 08:38:10 -0300

    feat: criando teste de criação de trannsação.

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,5 +1,23 @@
-import { test } from 'vitest'
+import { test, beforeAll, afterAll } from 'vitest'
+// @ts-ignore
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('o usuário consegue criar uma nova transação.', () => {
+beforeAll(async () => {
+	await app.ready()
+})
 
+afterAll(async () => {
+	await app.close()
+})
+
+test('user can create a new transaction', async () => {
+	await request(app.server)
+		.post('/transaction')
+		.send({
+			title: 'New transaction',
+			amount: 5000,
+			type: 'credit',
+		})
+		.expect(201)
 })
