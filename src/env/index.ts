2c6d2b1259c9ef01ef0950cc87d4276d7cd764a9
wr2net <wagnerr2@gmail.com>
2023-11-29 08:06:03 -0300

    feat: environments validations

---
+++ src/env/index.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+	NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+	PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+	const error = 'Invalid environments variables'
+	console.log(error, _env.error.format())
+
+	throw new Error(error)
+}
+
+export const env = _env.data
